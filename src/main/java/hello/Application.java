5bbcba1950a0c33d8aab16f15883f3a5feeb6bbe
Jonnlagadda <jjonna@a483e79a66f9.ant.amazon.com>
2019-10-25 14:46:05 -0400

    Correcting namespace in jenkins config file

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World from Tenant Three..!!";
+        return "Hello Docker World from Tenant Three..  3   !!";
     }
 
     public static void main(String[] args) {
